4552852eddfdb110f9de99dfd48d9d7a6592d947
jayuth <teerapong.uthairat.1877@gmail.com>
2019-08-18 15:35:40 -0400

    add an example of interface testability

---
+++ oop-principles/InterfacesTestability/InterfacesTestability/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InterfacesTestability
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
