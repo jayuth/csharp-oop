827103b1a4a1e8ba6d0106000e7c19cbf322748a
jayuth <teerapong.uthairat.1877@gmail.com>
2019-08-28 15:00:06 -0400

    add a new interface class, IDigitalProductModel

---
+++ oop-principles/ComprehensiveInterface/ComprehensiveInterface/IDigitalProductModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace ComprehensiveInterface
+{
+    // by implementing IProductModel, it allows IDigitalProductModel to have access to all properties and method in IProductModel
+    public interface IDigitalProductModel : IProductModel
+    {
+        int TotalDownloadsLeft { get; }
+    }
+}
